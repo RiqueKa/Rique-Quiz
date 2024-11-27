b0f5135a3021b8e687c7f00cff486c20b47c5164
RiqueKa <0000110696553xsp@al.educacao.sp.gov.br>
2024-11-27 08:03:25 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,70 @@
+criaCartao(
+    'Futebol',
+    'Qual é o melhor jogador de futebol do mundo?',
+    'O melhor jogador se chama Neymar Da Silva Santos Junior'
+)
+
+criaCartao(
+    'Kauan',
+    'Qual é a comida favorita de Kauan Rique ?',
+    'Strogonoff'
+)
+
+criaCartao(
+    'Geografia',
+    'qual é a capital da Lituania?',
+    'Vilnius'
+)
+
+criaCartao(
+    'Idioma',
+    'Qual é o idioma oficial usado em Cazaquistäo?',
+    'Cazaque e Russo'
+)
+
+criaCartao(
+    'Historia Futebol',
+    'Qual último time brasileiro a ganhar o mundial de clubes?',
+    'Sport Club Corinthians Paulista'
+)
+
+criaCartao(
+    'Comida',
+    'Do que é feito o salame?',
+    'carne de porco picada e temperada com uma mistura de especiarias e sal'
+)
+
+criaCartao(
+    'Geografia',
+    'Em que local fica o Brasil?',
+    'Planeta Terra na América do Sul'
+)
+
+criaCartao(
+    'Esportes',
+    'qual é o maior boxeador?',
+    'Floyd Mayweather'
+)
+criaCartao(
+    'Culinária',
+    'Como que faz pra empanar um frango?',
+    'primeiro vem a farinha de trigo, depois o ovo batido e, por último, a farinha de rosca'
+)
+
+criaCartao(
+    'Programação',
+    'O que é Java Scrip?',
+    'Linguagem de Programação'
+)
+
+criaCartao(
+    'Conhecimentos Gearais',
+    'qual é o viläo de chapéuzinho vermelho?',
+    'O lobo mau'
+)
+
+criaCartao(
+    'Musical',
+    'qual é a melhor banda de Rock?',
+    'Guns N Roses'
+)
